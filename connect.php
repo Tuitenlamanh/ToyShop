1ba3d0b2a1d592f715c8262f3c6b8f64df8e399e
Tuitenlamanh <manhvannguyen1998@gmail.com>
2022-04-17 20:46:57 +0700

    second edit

--- connect.php
+++ connect.php
@@ -1,15 +1,8 @@
 <?php
-<<<<<<< HEAD
     $severname = "3.132.234.157";
     $username = "manh79";
     $password = "123@123a";
     $database = "toyshop";
-=======
-    $severname = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "toyshop1";
->>>>>>> 6d4975fed69215950c9a6ebca1a43c908c042bb3
     //Khai báo biến để kết nỗi CSDL
     $connect = new mysqli($severname,$username,$password,$database);
     //$connect = new mysqli_connect ----> Hướng Thủ Tục
@@ -21,8 +14,5 @@
         {
             
         }
-<<<<<<< HEAD
-=======
 
->>>>>>> 6d4975fed69215950c9a6ebca1a43c908c042bb3
 ?>
