3ad03bcf886f1131fae14a3e331ea59e7bff3e48
Tuitenlamanh <manhvannguyen1998@gmail.com>
2022-04-17 22:52:44 +0700

    dm huy 3

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $severname = "3.132.234.157";
     $username = "manh79";
     $password = "123@123a";
-    $database = "manhdata";
+    $database = "toyshop";
     //Khai báo biến để kết nỗi CSDL
     $connect = new mysqli($severname,$username,$password,$database);
     //$connect = new mysqli_connect ----> Hướng Thủ Tục
